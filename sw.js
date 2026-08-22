b6aae82d2dfd7cf2436bea9cef6e2f66d9f0e1e8
takumi-0514 <takumikuro0514@gmail.com>
2026-08-22 09:50:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,6 +24,18 @@ self.addEventListener("activate", event => {
   );
 });
 
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientList => {
+      for (const client of clientList) {
+        if ("focus" in client) return client.focus();
+      }
+      if (self.clients.openWindow) return self.clients.openWindow("./");
+    })
+  );
+});
+
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(
